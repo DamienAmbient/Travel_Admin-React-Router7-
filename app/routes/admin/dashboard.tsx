8953160d268e85ca2215aedcb180308eea6ab7e5
Damien <testingmern7@gmail.com>
2025-05-08 17:54:44 +0100

    branch test

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <main className="dashboard wrapper">
       <Header
         title={`Welcome ${user?.name ?? "Guest"}`}
-        description="Track activity, trends and popular destinaytions in real time"
+        description="Track activity, trends and popular destinations in real time :)"
       ></Header>
       Dashboard Page Contents
     </main>
