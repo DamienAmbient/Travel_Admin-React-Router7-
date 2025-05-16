e660a3d6607f7be9fc8864f7b795e39fac1a7799
Damien <testingmern7@gmail.com>
2025-05-16 22:27:08 +0100

    :Stats card graphic layout

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,4 +1,4 @@
-import { calculateTrendPercentage } from "~/lib/utils";
+import { calculateTrendPercentage, cn } from "~/lib/utils";
 
 const StatsCard = ({
   headerTitle,
@@ -13,7 +13,47 @@ const StatsCard = ({
 
   const isDecrement = trend === "decrement";
 
-  return <div>StatsCard</div>;
+  return (
+    <article className="text-card">
+      <h3 className="text-base font-medium">{headerTitle}</h3>
+
+      <div className="content">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-4xl">{total}</h2>
+
+          <div className="flex items-center gap-2">
+            <figure className="flex items-center gap-1">
+              <img
+                src={`/assets/icons/${
+                  isDecrement ? "arrow-down-red.svg" : "arrow-up-green.svg"
+                }`}
+                alt="arrow"
+                className="size-5"
+              />
+              <figcaption
+                className={cn(
+                  "text-sm font-medium",
+                  isDecrement ? "text-red-500" : "text-success-700"
+                )}
+              >
+                {Math.round(percentage)}%
+              </figcaption>
+            </figure>
+            <p className="text-sm font-medium text-gray-100 truncate">
+              vs last month
+            </p>
+          </div>
+        </div>
+
+        <img
+          src={`/assets/icons/${
+            isDecrement ? "decrement.svg" : "increment.svg"
+          }  `}
+          alt=""
+        />
+      </div>
+    </article>
+  );
 };
 
 export default StatsCard;
